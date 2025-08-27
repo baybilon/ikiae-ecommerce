4e4c06c5fab20f422447c304968214ccbd4f88f2
Iqbal Pratama Yuda Putra <baybilon_iqbal@hotmail.com>
2025-08-27 18:10:50 +0700

    home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 // export default function Home() {
 //   return (
@@ -113,11 +114,15 @@ export default function Home() {
         <div>
           <Button variant="elevated">Tombol</Button>
         </div>
+        <div></div>
         <div>
           <Progress value={50} />
         </div>
         <div>
           <Input placeholder="inputan" />
+        </div>
+        <div>
+          <Textarea placeholder="inputan" />
         </div>
       </div>
     </div>
