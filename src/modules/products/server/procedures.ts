175e11c3bcf0cc568a8a8b805b838d7b7219bd8d
baybilon <baybilon_iqbal@hotmail.com>
2025-09-17 11:12:58 +0700

    add filtering

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -8,10 +8,24 @@ export const productsRouter = createTRPCRouter({
     .input(
       z.object({
         category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
       })
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
+
+      if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      }
+
+      if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
+      }
 
       if (input.category) {
         const categoriesData = await ctx.payload.find({
@@ -30,7 +44,6 @@ export const productsRouter = createTRPCRouter({
           ...doc,
           subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
             ...(doc as Category),
-            subcategories: undefined,
           })),
         }));
 
@@ -43,10 +56,10 @@ export const productsRouter = createTRPCRouter({
               (subcategory) => subcategory.slug
             )
           );
+          where["category.slug"] = {
+            in: [parentCategory.slug, ...subcategoriesSlugs],
+          };
         }
-        where["category.slug"] = {
-          in: [parentCategory.slug, ...subcategoriesSlugs],
-        };
       }
 
       const data = await ctx.payload.find({
