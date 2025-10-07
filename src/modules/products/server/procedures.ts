9eb7859ebd8eab48d6c3a01bcca8c8bdbcc59dfd
baybilon <baybilon_iqbal@hotmail.com>
2025-10-07 14:29:09 +0700

    access-control

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -21,6 +21,9 @@ export const productsRouter = createTRPCRouter({
         collection: "products",
         id: input.id,
         depth: 2, //load the "product.image", product.tenant, and product.image
+        select: {
+          content: false,
+        },
       });
 
       let isPurchased = false;
@@ -198,6 +201,9 @@ export const productsRouter = createTRPCRouter({
         sort,
         page: input.cursor,
         limit: input.limit,
+        select: {
+          content: false,
+        },
       });
 
       const dataWithSummarizedReviews = await Promise.all(
